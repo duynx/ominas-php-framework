01ee32068a0751c3e602ade86fea9cf42c5c844f
Duy Nguyen <duynx04@gmail.com>
2024-01-12 12:38:41 +0100

    Pretty URLs

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$action = $_GET['action'];
-$controller = $_GET['controller'];
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$segments = explode("/", $path);
+$action = $segments[2];
+$controller = $segments[1];
 
 require "src/controllers/$controller.php";
-
 $controller_object = new $controller;
-
 $controller_object->$action();
