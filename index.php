ecc82398a1cd81b98eb8f086dcbfd4072f1195cb
Duy Nguyen <duynx04@gmail.com>
2024-01-12 13:30:05 +0100

    Routing Basic

--- index.php
+++ index.php
@@ -4,9 +4,21 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+require "src/router.php";
+$router = new Router();
+$router->add("/home/index", ["controller" => "home", "action" => "index"]);
+$router->add("/products", ["controller" => "products", "action" => "index"]);
+$router->add("/", ["controller" => "home", "action" => "index"]);
+
+$params = $router->match($path);
+
+if($params === false)
+    exit("No route matched");
+
 $segments = explode("/", $path);
-$action = $segments[2];
-$controller = $segments[1];
+$action = $params["action"];
+$controller = $params["controller"];
 
 require "src/controllers/$controller.php";
 $controller_object = new $controller;
