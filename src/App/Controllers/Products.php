9f5a9d05f7f99223b6adc80cfc602726a0c17fa2
Duy Nguyen <duynx04@gmail.com>
2024-01-17 22:49:57 +0100

    Model - Updating and Deleting Records

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -39,6 +39,38 @@ class Products
         ]);
     }
 
+    public function edit(string $id)
+    {
+        $product = $this->model->find($id);
+
+        if ($product === false) {
+
+            throw new PageNotFoundException("Product not found");
+
+        }
+
+        echo $this->viewer->render("Layout/header.php", [
+            "title" => "Edit Product"
+        ]);
+
+        echo $this->viewer->render("Products/edit.php", [
+            "product" => $product
+        ]);
+    }
+
+    private function getProduct(string $id): array
+    {
+        $product = $this->model->find($id);
+
+        if ($product === false) {
+
+            throw new PageNotFoundException("Product not found");
+
+        }
+
+        return $product;
+    }
+
     public function showPage(string $title, string $id, string $page)
     {
         echo $title, " ", $id, " ", $page;
@@ -72,5 +104,53 @@ class Products
                 "errors" => $this->model->getErrors()
             ]);
         }
+    }
+
+    public function update(string $id)
+    {
+        $product = $this->getProduct($id);
+
+        $product["name"] = $_POST["name"];
+        $product["description"] = empty($_POST["description"]) ? null : $_POST["description"];
+
+        if ($this->model->update($id, $product)) {
+
+            header("Location: /products/{$id}/show");
+            exit;
+
+        } else {
+
+            echo $this->viewer->render("Layout/header.php", [
+                "title" => "Edit Product"
+            ]);
+
+            echo $this->viewer->render("Products/edit.php", [
+                "errors" => $this->model->getErrors(),
+                "product" => $product
+            ]);
+
+        }
+    }
+
+    public function delete(string $id)
+    {
+        $product = $this->getProduct($id);
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+            $this->model->delete($id);
+
+            header("Location: /products/index");
+            exit;
+
+        }
+
+        echo $this->viewer->render("Layout/header.php", [
+            "title" => "Delete Product"
+        ]);
+
+        echo $this->viewer->render("Products/delete.php", [
+            "product" => $product
+        ]);
     }
 }
--- src/Framework/Model.php
+++ src/Framework/Model.php
@@ -11,7 +11,55 @@ abstract class Model
 
     protected array $errors = [];
 
-    protected function validate(array $data): void {}
+    public function update(string $id, array $data): bool
+    {
+        $this->validate($data);
+
+        if ( ! empty($this->errors)) {
+            return false;
+        }
+
+        $sql = "UPDATE {$this->getTable()} ";
+
+        unset($data["id"]);
+
+        $assignments = array_keys($data);
+
+        array_walk($assignments, function (&$value) {
+            $value = "$value = ?";
+        });
+
+        $sql .= " SET " . implode(", ", $assignments);
+
+        $sql .= " WHERE id = ?";
+
+        $conn = $this->database->getConnection();
+
+        $stmt = $conn->prepare($sql);
+
+        $i = 1;
+
+        foreach ($data as $value) {
+
+            $type = match(gettype($value)) {
+                "boolean" => PDO::PARAM_BOOL,
+                "integer" => PDO::PARAM_INT,
+                "NULL" => PDO::PARAM_NULL,
+                default => PDO::PARAM_STR
+            };
+
+            $stmt->bindValue($i++, $value, $type);
+
+        }
+
+        $stmt->bindValue($i, $id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
+
+    protected function validate(array $data): void
+    {
+    }
 
     public function getInsertID(): string
     {
@@ -32,8 +80,10 @@ abstract class Model
 
     private function getTable(): string
     {
-        if($this->table !== null) {
+        if ($this->table !== null) {
+
             return $this->table;
+
         }
 
         $parts = explode("\\", $this::class);
@@ -41,22 +91,34 @@ abstract class Model
         return strtolower(array_pop($parts));
     }
 
-    public function __construct(private Database $database) {}
+    public function __construct(protected Database $database)
+    {
+    }
 
     public function findAll(): array
     {
         $pdo = $this->database->getConnection();
-        $sql = "SELECT * FROM {$this->getTable()}";
 
-        return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $sql = "SELECT *
+                FROM {$this->getTable()}";
+
+        $stmt = $pdo->query($sql);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function find(string $id): array|bool
     {
         $conn = $this->database->getConnection();
-        $sql = "SELECT * FROM {$this->getTable()} WHERE id = :id";
+
+        $sql = "SELECT *
+                FROM {$this->getTable()}
+                WHERE id = :id";
+
         $stmt = $conn->prepare($sql);
+
         $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+
         $stmt->execute();
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
@@ -66,7 +128,7 @@ abstract class Model
     {
         $this->validate($data);
 
-        if(!empty($this->errors)){
+        if ( ! empty($this->errors)) {
             return false;
         }
 
@@ -94,6 +156,20 @@ abstract class Model
             $stmt->bindValue($i++, $value, $type);
 
         }
+
+        return $stmt->execute();
+    }
+
+    public function delete(string $id): bool
+    {
+        $sql = "DELETE FROM {$this->getTable()}
+                WHERE id = :id";
+
+        $conn = $this->database->getConnection();
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
 
         return $stmt->execute();
     }
